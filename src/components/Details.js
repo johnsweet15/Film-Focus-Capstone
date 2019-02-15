71515beecfae094f81d1f7ea07fce7396b3c2921
John Sweet <john.sweet.15@cnu.edu>
2019-02-15 00:47:41 -0500

    added cast list to details page in a slideshow

--- src/components/Details.js
+++ src/components/Details.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import Slider from 'react-slick';
 // import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 
 const TMDBKey = 'c794333156e1c095f41f92e128c002df';
@@ -14,6 +15,7 @@ class Details extends Component {
       // see onClick method on lines 100 and 120 in movies.js that sends data to app.js
       // then get that movie from app.js through props
       movie: this.props.movie,
+      cast: []
     }
 
   }
@@ -34,21 +36,33 @@ class Details extends Component {
       // needed to check if state has been updated in render()
       movie.updated = true;
 
-      this.setState({
-        movie: movie
-      })
-      
+      this.setState({movie: movie})
+    })
+
+    // get cast
+    axios.get('https://api.themoviedb.org/3/' + movie.media_type + '/' + movie.id + '/credits?api_key=' + TMDBKey)
+    .then((response) => {
+      let cast = response.data.cast;
+
+      this.setState({cast: cast});
     })
   }
 
   render() {
     let movie = this.state.movie;
+    let cast = this.state.cast;
 
     // check for updated movie object
     if(movie.updated === undefined) {
       console.log('returned null');
       return null;
     }
+
+    if(cast.length === 0) {
+      return null;
+    }
+
+    console.log('cast: ' + cast.length);
 
     console.log('details movie: ' + this.state.movie.in_production);
     let releaseDate = '';
@@ -69,9 +83,27 @@ class Details extends Component {
         finishDate = movie.last_air_date.substring(0,4);
       }
     }
-    
+
+    let castList = this.state.cast.map(actor => {
+      return (
+        <div style={{padding: 3}}>
+          <img style={{width: '10vw', alignSelf: 'center'}} src={'https://image.tmdb.org/t/p/w600_and_h900_bestv2' + actor.profile_path} alt='' />
+          <p style={{color: 'white'}}>{actor.name}</p>
+        </div>
+      );
+    })
+
+    var settings = {
+      dots: true,
+      infinite: true,
+      speed: 500,
+      slidesToShow: 4,
+      slidesToScroll: 4,
+      adaptiveHeight: true
+    };
+
     return (
-      <div style={{minHeight: '100vh', backgroundColor: '#282c34'}}>
+      <div style={{minHeight: '100vh', backgroundColor: '#282c34', maxWidth:'100vw'}}>
         <div style={{textAlign: 'center', color: '#4286f4'}}>
           <h1 style={styles.header}>Details</h1>
         </div>
@@ -91,13 +123,21 @@ class Details extends Component {
                 <p style={{color: 'white', fontSize: '3vh'}}>{movie.title + ' (' + releaseDate.substring(0,4) + ')'}</p>
               }
               <p style={{color: 'white', fontSize: '2vh'}}>{movie.overview}</p>
+              <div style={{width: '100%', padding: 5}}>
+                <h2 style={{color: 'white', fontSize: '3vh'}}>Cast</h2>
+                <div style={{margin: 0, padding: 0, width: '45vw'}}>
+                  {cast.length > 0 &&
+                    <Slider {...settings}>
+                      {castList}
+                    </Slider>
+                  }
+                </div>
+              </div>
             </div>
-
           </div>
-          
         </div>
-    
       </div>
+      
     );
   }
 }
@@ -112,7 +152,13 @@ const styles = {
     fontSize: '5vh',
     margin: 0,
     padding: 40
+  },
+
+  castImage: {
+    width: '10%'
   }
 }
+
+
 
 export default Details;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+// import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
