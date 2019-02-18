7b58b5741a9dad3ecbafebac8399995794188719
Aaron Redman <aaron.redman.15@cnu.edu>
2019-02-17 22:23:19 -0500

    Modified gitignore

--- src/constants.js
+++ src/constants.js
@@ -1,6 +1,6 @@
 const APIKey = 'a6793cf9';
 const TMDBKey = 'c794333156e1c095f41f92e128c002df';
 const FeaturedURL = 'https://api.themoviedb.org/3/movie/popular?api_key=';
-const YouTubeKey = 'heyo';
+const YouTubeKey = 'testing';
 
 export default {APIKey, TMDBKey, FeaturedURL, YouTubeKey};
