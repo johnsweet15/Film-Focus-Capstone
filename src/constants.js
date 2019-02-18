dda81e91817bf75f6b16210d141cfb84c2fb76aa
Aaron Redman <aaron.redman.15@cnu.edu>
2019-02-17 22:37:36 -0500

    Added protection to keys, fixed constants

--- src/constants.js
+++
@@ -1,6 +0,0 @@
-const APIKey = 'a6793cf9';
-const TMDBKey = 'c794333156e1c095f41f92e128c002df';
-const FeaturedURL = 'https://api.themoviedb.org/3/movie/popular?api_key=';
-const YouTubeKey = 'testing';
-
-export default {APIKey, TMDBKey, FeaturedURL, YouTubeKey};
