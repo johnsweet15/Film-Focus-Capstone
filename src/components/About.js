21fb1e0c1e4e7b3e5d2d9c736e59f1c3ecd5a713
Aaron Redman <aaron.redman.15@cnu.edu>
2019-04-14 19:32:13 -0400

    Updated the about page

--- src/components/About.js
+++ src/components/About.js
@@ -15,7 +15,10 @@ export default class About extends Component {
         <h1 className='header' id='header'>About</h1>
         <br></br>
         <p style={{color: 'white', fontSize: '2vh', textAlign: 'center'}}>Thanks to <a href='https://www.themoviedb.org/' target="_blank">The Movie DB</a></p>
-        <p style={{color: 'white', fontSize: '2vh', textAlign: 'center'}}>[Description of the site]</p>
+        <p style={{color: 'white', fontSize: '2vh', textAlign: 'center'}}>This website aims to correct some of the flaws of current movie databases, while introducing
+        some features that we feel are lacking in the current options. We hope it appeals to most movie-
+        goers, especially avid movie fans and people who frequently search for information about
+        movies – providing relevant information in an easy-to-read format.</p>
       </div>
     );
   }
